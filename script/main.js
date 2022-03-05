4f17535b10d787f2e18a24423b8c7818c575ad43
Gonzalo <gonzomartinarango@gmail.com>
2022-03-05 10:07:01 -0500

    Background Change JavaScript done

--- script/main.js
+++ script/main.js
@@ -1,4 +1,18 @@
 (() => {
-	// set up the puzzle pieces and boards
-	
+	// Make the connections to the elements on the page that we wnat the user to interact with
+	const theButtons = document.querySelectorAll('#buttonHolder img'),
+				theGameBoard = document.querySelector('.puzzle-board');
+
+	// theButtons becomes a container for each img on the HTML.
+	function changeBgImg() {
+		// debugger;
+		// let key = this.dataset.bgref;
+		// console.log(key);
+
+		theGameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`;
+	}
+
+	theButtons.forEach(button => button.addEventListener("click", changeBgImg));
+
+
 })();
